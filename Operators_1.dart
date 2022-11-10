89f4a82610fee453e00c2fc95bb4eb28c358f571
Dhorajiya Nency <109361169+DhorajiyaNency@users.noreply.github.com>
2022-11-10 17:27:15 +0530

    10/11/2022

---
+++ Operators_1.dart
@@ -0,0 +1,69 @@
+void main() {
+  int eng = 60, maths = 65;
+//    (eng>50)?print("you are eligible"):print("you are not eligible");
+  (eng > maths)
+      ? print("your english score is greter than maths")
+      : print("your english score is less than maths");
+}
+
+// void main() {
+//   int A = 30, B = 40, C = 10;
+//   // (A>B&&A>C)?A:(B>C)?B:C;
+//   print("Largest Value among A B and C is ...");
+//     (A>B&&A>C)?print("A = $A"):(B>C)?print("B = $B"):print("C = $C");
+
+// }
+
+
+
+// void main()
+// {
+//   int bankBalance=1000;
+//   print(bankBalance-=800);  // (/,%) are not , problem null safty
+// }
+
+
+
+// void main()
+// {
+//   double var1=4.55,var2=9.99;
+//   if(var1>=var2)
+//   {
+//     print("var1 = $var1 is greter than or equal to var2 = $var2");
+//   }
+//   else
+//   {
+//     print("var1 = $var1 is less than or equal to var2 = $var2");
+//   }
+// }
+
+
+// void main()
+// {
+//   String str1="nency",str2="Dhorajiya";
+//   if(str1==str2)
+//   {
+//     print("both name are same");
+//   }
+//   else
+//   {
+//     print("both name are not same");
+//   }
+// }
+
+
+
+// 1.
+// // relational operator
+// void main()
+// {
+//   int var1=30,var2=40;
+//   if(var1<var2)
+//   {
+//     print("var1 is less than var2");
+//   }
+//   else
+//   {
+//     print("var1 is getter than var2");
+//   }
+// }
